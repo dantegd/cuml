9a80d471474f975f2319e36542c837b942760a27
Dante Gama Dessavre <danteg@nvidia.com>
2023-09-26 15:33:36 -0700

    DBG2

--- conda/recipes/cuml/build.sh
+++ conda/recipes/cuml/build.sh
@@ -2,4 +2,6 @@
 # Copyright (c) 2018-2022, NVIDIA CORPORATION.
 
 # This assumes the script is executed from the root of the repo directory
+
+echo "123123123123123-231321321321321"
 ./build.sh cuml -v
--- conda/recipes/libcuml/build.sh
+++ conda/recipes/libcuml/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Copyright (c) 2018-2022, NVIDIA CORPORATION.
 
+echo "GFDGg54g54g54g54g54g54g54g54g54g54g545g54g"
 ./build.sh -n libcuml prims -v --allgpuarch
